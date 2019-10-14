bbeaabe77c5af6e640a6f9b6f7645e946e4e0494
AAYamoldin <aayamoldin@gmail.com>
2019-10-14 19:20:47 +0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_comfirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
