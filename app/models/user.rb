e899b06bd1c243afa02422f47e2c792c1ec7984e
AAYamoldin <aayamoldin@gmail.com>
2019-10-14 17:52:06 +0300

    Finish user singup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ApplicationRecord
   before_save {email.downcase!}
-validates :name,:email, presence: true
-validates :name, length: {maximum: 50}, presence: true
-validates :email, length: {maximum: 255}, presence: true
+validates :name,length: {maximum: 50},  presence: true
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, format: {with: VALID_EMAIL_REGEX}, presence: true
-validates :email, uniqueness: true, :presence => true
+  validates :email, format: {with: VALID_EMAIL_REGEX},uniqueness: true, length: {maximum: 255}, presence: true
 has_secure_password
 validates :password, length: {minimum: 6}, presence: true
 end
